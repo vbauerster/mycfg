403308f2a113e380b90510453c8a64988217f22f
Vladimir Bauer <vbauerster@gmail.com>
2019-04-03 18:51:09 +0300

    my zshrc

---
+++ .myfuncs.zsh
@@ -0,0 +1,278 @@
+# GIT heart FZF
+# http://junegunn.kr/2016/07/fzf-git
+# -------------
+
+fzf-down() {
+  fzf --height 50% "$@" --border
+}
+
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+fzf-gt() {
+  is_in_git_repo || return
+  git tag --sort -version:refname |
+  fzf-down --multi --preview-window right:70% \
+    --preview 'git show --color=always {} | head -200'
+}
+
+fzf-gh() {
+  is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -200' |
+  grep -o "[a-f0-9]\{7,\}"
+}
+
+fzf-gf() {
+  is_in_git_repo || return
+  git -c color.status=always status --short |
+  fzf-down -m --ansi --nth 2..,.. \
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+  cut -c4- | sed 's/.* -> //'
+}
+
+fzf-gb() {
+  is_in_git_repo || return
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  fzf-down --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -200' |
+  sed 's/^..//' | cut -d' ' -f1 |
+  sed 's#^remotes/##'
+}
+
+fzf-gr() {
+  is_in_git_repo || return
+  git remote -v | awk '{print $1 "\t" $2}' | uniq |
+  fzf-down --tac \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+  cut -d$'\t' -f1
+}
+
+fzf-gs() {
+  is_in_git_repo || return
+  git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1}' |
+  cut -d: -f1
+}
+
+# A helper function to join multi-line output from fzf
+# join-lines() {
+# local item
+# while read item; do
+#   echo -n "${(q)item} "
+# done
+# }
+
+fzf-gt-widget() {
+    LBUFFER+=$(fzf-gt)
+    zle reset-prompt
+}
+zle -N fzf-gt-widget
+
+fzf-gh-widget() {
+    LBUFFER+=$(fzf-gh)
+    zle reset-prompt
+}
+zle -N fzf-gh-widget
+
+fzf-gf-widget() {
+    LBUFFER+=$(fzf-gf)
+    zle reset-prompt
+}
+zle -N fzf-gf-widget
+
+fzf-gb-widget() {
+    LBUFFER+=$(fzf-gb)
+    zle reset-prompt
+}
+zle -N fzf-gb-widget
+
+fzf-gr-widget() {
+    LBUFFER+=$(fzf-gr)
+    zle reset-prompt
+}
+zle -N fzf-gr-widget
+
+fzf-gs-widget() {
+    LBUFFER+=$(fzf-gs)
+    zle reset-prompt
+}
+zle -N fzf-gs-widget
+
+# Figlet font selector => copy to clipboard
+fgl() (
+  [ $# -eq 0 ] && return
+  cd /usr/local/Cellar/figlet/*/share/figlet/fonts
+  local font=$(ls *.flf | sort | fzf --no-multi --reverse --preview "figlet -f {} $@") &&
+  figlet -f "$font" "$@" | pbcopy
+)
+
+# fzf related functions
+# ---------------------
+# gco - checkout git branch/tag
+gco() {
+  local tags branches target
+  tags=$(
+    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+  branches=$(
+    git branch --all | grep -v HEAD             |
+    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+  target=$(
+    (echo "$tags"; echo "$branches") |
+    fzf-tmux -l40 -- --no-hscroll --ansi +m -d "\t" -n 2 -1 -q "$*") || return
+  git checkout $(echo "$target" | awk '{print $2}')
+}
+
+# ghh - git history browser
+ghh() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --header "Press CTRL-S to toggle sort" \
+      --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
+                 xargs -I % sh -c 'git show --color=always % | head -200 '" \
+      --bind "enter:execute:echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
+              xargs -I % sh -c 'nvim fugitive://\$(git rev-parse --show-toplevel)/.git//% < /dev/tty'"
+}
+
+# tags - search ctags
+ftags() {
+  local line
+  [ -e tags ] &&
+  line=$(
+    awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
+    cut -c1-$COLUMNS | fzf --nth=2 --tiebreak=begin
+  ) && $EDITOR $(cut -f3 <<< "$line") -c "set nocst" \
+                                      -c "silent tag $(cut -f2 <<< "$line")"
+}
+
+# ee [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+ee() {
+  local file
+  file=$(fzf-tmux --query="$1" --select-1 --exit-0)
+  [ -n "$file" ] && ${EDITOR:-vim} "$file"
+}
+
+if [ -n "$TMUX_PANE" ]; then
+  # https://github.com/wellle/tmux-complete.vim
+  fzf_tmux_words() {
+    tmuxwords.rb --all --scroll 500 --min 5 | fzf-down --multi | paste -sd" " -
+  }
+  fzf-tmux-words-widget() {
+    LBUFFER+=$(fzf_tmux_words)
+    zle reset-prompt
+  }
+  zle -N fzf-tmux-words-widget
+  bindkey '^X^T' fzf-tmux-words-widget
+  # bindkey '^T' undefined-key
+  # bindkey '^Tt' fzf-file-widget
+  # bindkey '^Tw' fzf-tmux-words-widget
+
+  # tmps - switch pane (@george-b)
+  tmps() {
+    local panes current_window current_pane target target_window target_pane
+    panes=$(tmux list-panes -s -F '#I:#P - #{pane_current_path} #{pane_current_command}')
+    current_pane=$(tmux display-message -p '#I:#P')
+    current_window=$(tmux display-message -p '#I')
+
+    target=$(echo "$panes" | grep -v "$current_pane" | fzf +m --reverse) || return
+
+    target_window=$(echo $target | awk 'BEGIN{FS=":|-"} {print$1}')
+    target_pane=$(echo $target | awk 'BEGIN{FS=":|-"} {print$2}' | cut -c 1)
+
+    if [[ $current_window -eq $target_window ]]; then
+      tmux select-pane -t ${target_window}.${target_pane}
+    else
+      tmux select-pane -t ${target_window}.${target_pane} &&
+      tmux select-window -t $target_window
+    fi
+  }
+
+fi
+
+# Switch tmux-sessions
+tmss() {
+  local session
+  session=$(tmux list-sessions -F "#{session_name}" | \
+    fzf --height 40% --reverse --query="$1" --select-1 --exit-0) &&
+  tmux switch-client -t "$session"
+}
+
+# some custotm functions
+# ----------------------
+
+light() {
+    export BACKGROUND="light"
+}
+
+dark() {
+    export BACKGROUND="dark"
+}
+
+# print available colors and their numbers
+colors() {
+    for i in {0..255}; do
+        printf "\x1b[38;5;${i}m colour${i}"
+        if (( $i % 5 == 0 )); then
+            printf "\n"
+        else
+            printf "\t"
+        fi
+    done
+}
+
+# https://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity
+fancy-ctrl-z() {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
+
+# Song Trimmer
+# trimmer <song> <start(sec)> <duration(sec)>
+trimmer() {
+  ffmpeg -i "$1" -ss "$2" -t "$3" -acodec copy cut_"$1"
+}
+
+Fkeys() {
+  printf "%-5s%5s\n" "key" "value"; infocmp -1  | awk -F= '/kf/ { key=$1; sub("kf", "", key); printf("%-5d %s\n", key, $2) }'  | sort -n
+}
+
+csi() {
+  echo -en "\x1b[$*"
+}
+
+..cd() {
+    cd ..
+    cd "$@"
+}
+
+# Go up an abritrary number of directories
+# Use with 'up' or 'up 5' to go up N directories
+up() {
+    if [[ "$#" < 1 ]] ; then
+        cd ..
+    else
+        CDSTR=""
+        for i in {1..$1} ; do
+            CDSTR="../$CDSTR"
+        done
+        cd $CDSTR
+    fi
+}
+
+# http://artkoshelev.github.io/posts/sed-for-configs
+catconf() {
+    cat "$@" | sed '/ *#/d; /^ *$/d'
+}
---
+++ .zshrc
@@ -0,0 +1,125 @@
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
+# use the Dvorak keyboard for the basis for examining spelling mistakes
+setopt DVORAK
+
+# history settings
+setopt EXTENDED_HISTORY    # writes the history file in the *:start:elapsed;command* format
+setopt INC_APPEND_HISTORY  # writes to the history file immediately, not when the shell exits
+setopt HIST_IGNORE_DUPS    # does not record an event that was just recorded again.
+setopt HIST_SAVE_NO_DUPS   # does not write a duplicate event to the history file
+setopt HIST_IGNORE_SPACE   # does not record an event starting with a space
+SAVEHIST=8192
+HISTSIZE=8192              # stores the maximum number of events to save in the internal history
+
+# The time the shell waits, in hundredths of seconds, (default is 40)
+# for another key to be pressed when reading bound multi-character sequences.
+# 100ms for key sequences
+export KEYTIMEOUT=10
+
+export BACKGROUND="light"
+
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
+# http://zsh.sourceforge.net/Guide/zshguide04.html
+# http://www.geekmind.net/2011/01/shortcuts-to-improve-your-bash-zsh.html
+# export DOTFILES="$HOME/dotfiles"
+
+# https://github.com/BurntSushi/ripgrep
+[ -f "$HOME/.rgrc" ] && export RIPGREP_CONFIG_PATH="$HOME/.rgrc"
+
+# https://github.com/junegunn/fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# export FZF_DEFAULT_COMMAND='ag --nocolor --hidden --ignore .git --ignore vendor -g ""'
+# export FZF_DEFAULT_COMMAND="rg -uu -g '!vendor' -g '!.git' --files"
+# export FZF_DEFAULT_COMMAND='pt --nocolor --hidden --home-ptignore -U -g ""'
+# [ -n "$NVIM_LISTEN_ADDRESS" ] && export FZF_DEFAULT_OPTS='--no-height'
+
+if [ ! -z "$(command -v fd)" ]; then
+    export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+    export FZF_CTRL_T_COMMAND='fd --type f --type d --hidden --follow --exclude .git'
+    # export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
+fi
+
+# https://github.com/junegunn/blsd
+command -v blsd > /dev/null && export FZF_ALT_C_COMMAND='blsd $dir'
+# brew install tree
+command -v tree > /dev/null && export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -n 200'"
+# brew install bat
+command -v bat > /dev/null && export FZF_CTRL_T_OPTS="--preview 'bat --color=always --style=plain {} | head -n 200'"
+
+# if [ -x ~/.config/nvim/plugged/fzf.vim/bin/preview.rb ]; then
+# 	export FZF_CTRL_T_OPTS="--preview '~/.config/nvim/plugged/fzf.vim/bin/preview.rb {} | head -200'"
+# fi
+
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview' --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' --header 'Press CTRL-Y to copy command into clipboard' --border"
+
+# check for user bin directory and add to path
+if [[ ! "$PATH" == *$HOME/bin* && -d $HOME/bin ]]; then
+    export PATH="$PATH:$HOME/bin"
+fi
+
+# Bindings
+# http://zsh.sourceforge.net/Intro/intro_11.html#SEC11
+# bindkey '^A' beginning-of-line
+# bindkey '^E' end-of-line
+# bindkey '^K' kill-line
+# bindkey '^[H' run-help
+# ctrl + enter = accept-and-hold
+bindkey '^[[13;5u' accept-and-hold
+# in vi mode use j/k for history-substring search
+# bindkey '^P' history-substring-search-up
+# bindkey '^N' history-substring-search-down
+bindkey '^[OA' up-line-or-history
+bindkey '^[OB' down-line-or-history
+# shift + tab
+# bindkey '^[[Z' autosuggest-accept
+
+# following just for reference
+# ^Q push-line-or-edit
+
+# history-incremental is from editor module
+# in vi mode / = history-incremental-search-forward
+# in vi mode ? = history-incremental-search-backward
+
+# To view all vicmd bindings: bindkey -M vicmd
+# bindkey -M vicmd '^U' vi-kill-line
+
+# fzf bindings
+# to view all: bindkey-all | rg -i fzf
+# http://junegunn.kr/2016/07/fzf-git/
+# first unbind '^G', which is bound to list-expand by default
+bindkey '^G' undefined-key
+# for tags
+bindkey '^Gt' fzf-gt-widget
+# for commit hashes
+bindkey '^Gh' fzf-gh-widget
+# for files
+bindkey '^Gf' fzf-gf-widget
+# for branches
+bindkey '^Gb' fzf-gb-widget
+# for remotes
+bindkey '^Gr' fzf-gr-widget
+# for stashes
+bindkey '^Gs' fzf-gs-widget
+
+# https://github.com/kurkale6ka/zsh/blob/master/.zshrc
+### ^x0 IPs
+bindkey -s '^x0' '127.0.0.1'
+bindkey -s '^x1' '10.0.0.'
+bindkey -s '^x7' '172.16.0.'
+bindkey -s '^x9' '192.168.0.'
+
+### ^x_ /dev/null
+bindkey -s '^x_' '/dev/null'
+
+# [ -f ~/.iterm2_shell_integration.zsh ] && source ~/.iterm2_shell_integration.zsh
+
+# Source functions
+[ -f ~/dotfiles/.myfuncs.zsh ] && source ~/dotfiles/.myfuncs.zsh
+
+# Source aliases
+[ -f ~/.aliases ] && source ~/.aliases
