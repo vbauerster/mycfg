2fd8e064818cd6fd511f1ed3d410d458b916a0c8
Vladimir Bauer <vbauerster@gmail.com>
2019-06-01 14:38:32 +0300

    hammerspoon: simplify numpad

--- .hammerspoon/numpad.lua
+++ .hammerspoon/numpad.lua
@@ -14,57 +14,40 @@ npad[25] = 'pad4'
 npad[29] = 'pad6'
 npad[27] = 'pad8'
 
-local register = function(key)
-    local modalKey = hs.hotkey.modal.new({'shift'}, key, 'Npad enter')
-    local doExit = function()
-        modalKey:exit()
-        hs.alert.show('Npad quit', 0.5)
-    end
-    local exitTimer = hs.timer.delayed.new(5, doExit)
-    modalKey:bind({}, 'escape', doExit)
-    function modalKey:entered()
-        exitTimer:start()
-        hs.eventtap.keyStroke({}, npad[key])
-    end
-    function modalKey:exited()
-        if exitTimer:running() then
-            exitTimer:stop()
-        end
-    end
+local modalKey = hs.hotkey.modal.new({'ctrl', 'alt', 'cmd'}, 'n', 'Npad on')
+local doExit = function()
+    modalKey:exit()
+    hs.alert.show('Npad off', 0.5)
+end
+modalKey:bind({}, 'escape', doExit)
 
-    for k, v in pairs(npad) do
-        modalKey:bind({'shift'}, k, function()
-            -- Pressed:
-            exitTimer:start()
-            hs.eventtap.event.newKeyEvent({}, v, true):post()
-        end, function()
-            -- Released:
-            hs.eventtap.event.newKeyEvent({}, v, false):post()
-        end, function()
-            -- Repeat:
-            hs.eventtap.event.newKeyEvent({}, v, true):setProperty(hs.eventtap.event.properties.keyboardEventAutorepeat, 1):post()
-        end)
-        modalKey:bind({}, k, function()
-            -- Pressed:
-            exitTimer:start()
-            hs.eventtap.event.newKeyEvent({}, v, true):post()
-        end, function()
-            -- Released:
-            hs.eventtap.event.newKeyEvent({}, v, false):post()
-        end, function()
-            -- Repeat:
-            hs.eventtap.event.newKeyEvent({}, v, true):setProperty(hs.eventtap.event.properties.keyboardEventAutorepeat, 1):post()
-        end)
-    end
-    return modalKey
+local exitTimer = hs.timer.delayed.new(4, doExit)
+
+modalKey:bind({'ctrl', 'alt', 'cmd'}, 'n', function() exitTimer:start() end)
+
+function modalKey:entered()
+    exitTimer:start()
 end
 
-local modals = {}
-
-if hs.keycodes.currentLayout() == "PDvorak" then
-    for k, v in pairs(npad) do
-            modals[k] = register(k)
+function modalKey:exited()
+    if exitTimer:running() then
+        exitTimer:stop()
     end
+end
+
+for k, v in pairs(npad) do
+    modalKey:bind({}, k, function()
+        -- Pressed:
+        hs.eventtap.event.newKeyEvent({}, v, true):post()
+    end, function()
+        -- Released:
+        hs.eventtap.event.newKeyEvent({}, v, false):post()
+        exitTimer:start()
+    end, function()
+        -- Repeat:
+        hs.eventtap.event.newKeyEvent({}, v, true):setProperty(hs.eventtap.event.properties.keyboardEventAutorepeat, 1):post()
+        exitTimer:start()
+    end)
 end
 
 -- https://github.com/Hammerspoon/hammerspoon/issues/1499
@@ -72,48 +55,9 @@ local foo = hs.distributednotifications.new(
     function(name, object, userInfo)
         -- print("notification: " .. name .. "\n")
         local currentLayout = hs.keycodes.currentLayout()
-        if currentLayout == "PDvorak" then
-            for k, v in pairs(npad) do
-                if modals[k] == nil then
-                    modals[k] = register(k)
-                end
-            end
-        else
-            for k, v in pairs(modals) do
-                if v ~= nil then
-                    v:exit()
-                    v:delete()
-                    modals[k] = nil
-                end
-            end
-        end
         hs.alert.show(currentLayout)
     end,
     -- or 'AppleSelectedInputSourcesChangedNotification'
     'com.apple.Carbon.TISNotifySelectedKeyboardInputSourceChanged'
 )
 foo:start()
-
--- local update = function()
---     local currentLayout = hs.keycodes.currentLayout()
---     if currentLayout == "PDvorak" then
---         -- log.i("OK PDvorak")
---         for k, v in pairs(npad) do
---             if modals[k] == nil then
---                 modals[k] = register(k)
---             end
---         end
---     else
---         -- log.i("NOT PDvorak")
---         for k, v in pairs(modals) do
---             if v ~= nil then
---                 v:delete()
---                 modals[k] = nil
---             end
---         end
---     end
---     hs.alert.show(currentLayout)
--- end
--- hs.keycodes.inputSourceChanged(update)
--- update()
-
